771195b781c9b6e5f39efe7fc336668b2bbe21b6
Lucas Garron <code@garron.net>
2025-09-17 21:00:33 -0700

    Format the generated JSON schema immediately after it is generated.
    This avoids surprises linting issues.

--- script/schema/update.ts
+++ script/schema/update.ts
@@ -1,9 +1,11 @@
 #!/usr/bin/env -S bun run --
 
-import { file } from "bun";
+import { $, file } from "bun";
 import { generateSchema } from "./generate";
 
 const definition = await generateSchema();
 await file("./src/lib/schema.json").write(
   JSON.stringify(definition, null, "  "),
 );
+
+await $`bun x @biomejs/biome check --write src/lib/schema.json`;
