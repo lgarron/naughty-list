22db83d479171fe5abae180b25c7a16916e7ebb5
Lucas Garron <code@garron.net>
2025-12-24 14:54:47 -0800

    Switch to Optique for commandline parsing.

--- src/bin/main.ts
+++ src/bin/main.ts
@@ -1,24 +1,44 @@
 #!/usr/bin/env -S bun run --
 
-import { binary, command, oneOf, option, optional, run } from "cmd-ts-too";
+import { argv } from "node:process";
+import {
+  choice,
+  constant,
+  message,
+  object,
+  option,
+  withDefault,
+} from "@optique/core";
+import { run } from "@optique/run";
+import { Path } from "path-class";
+import { version as VERSION } from "../../package.json";
 import {
   ON_UNKNOWN_BEHAVIOURS,
   ON_UNKNOWN_DEFAULT_BEHAVIOUR,
   sweep,
 } from "../lib/sweep";
 
-const app = command({
-  name: "naughty-list",
-  args: {
-    onUnknown: option({
-      description: `Output format. One of: ${ON_UNKNOWN_BEHAVIOURS.join(", ")} (default: ${ON_UNKNOWN_DEFAULT_BEHAVIOUR})`,
-      type: optional(oneOf(ON_UNKNOWN_BEHAVIOURS)),
-      long: "on-unknown",
-    }),
+const args = run(
+  object({
+    subcommand: constant("read"),
+    onUnknown: withDefault(
+      option("--on-unknown", choice(ON_UNKNOWN_BEHAVIOURS)),
+      ON_UNKNOWN_DEFAULT_BEHAVIOUR,
+    ),
+  }),
+  {
+    programName: new Path(argv[1]).basename.path,
+    description: message`The commandline tool of the future!`,
+    help: "option",
+    completion: {
+      mode: "option",
+      name: "plural",
+    },
+    version: {
+      mode: "option",
+      value: VERSION,
+    },
   },
-  handler: async ({ onUnknown }) => {
-    sweep({ onUnknown });
-  },
-});
+);
 
-await run(binary(app), process.argv);
+await sweep(args);
