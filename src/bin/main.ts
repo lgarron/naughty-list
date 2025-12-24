ff41dc8e7ca5f950b692a9e94c0c4116ac12a684
Lucas Garron <code@garron.net>
2025-12-24 15:07:39 -0800

    Fix the binary description and adjust the metavar for `--on-unknown`.

--- src/bin/main.ts
+++ src/bin/main.ts
@@ -11,7 +11,7 @@ import {
 } from "@optique/core";
 import { run } from "@optique/run";
 import { Path } from "path-class";
-import { version as VERSION } from "../../package.json";
+import { description, version as VERSION } from "../../package.json";
 import {
   ON_UNKNOWN_BEHAVIOURS,
   ON_UNKNOWN_DEFAULT_BEHAVIOUR,
@@ -22,13 +22,16 @@ const args = run(
   object({
     subcommand: constant("read"),
     onUnknown: withDefault(
-      option("--on-unknown", choice(ON_UNKNOWN_BEHAVIOURS)),
+      option(
+        "--on-unknown",
+        choice(ON_UNKNOWN_BEHAVIOURS, { metavar: "BEHAVIOUR" }),
+      ),
       ON_UNKNOWN_DEFAULT_BEHAVIOUR,
     ),
   }),
   {
     programName: new Path(argv[1]).basename.path,
-    description: message`The commandline tool of the future!`,
+    description: message`${description}`,
     help: "option",
     completion: {
       mode: "option",
